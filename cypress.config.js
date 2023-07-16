6ff6aaab424e23f4a91f0611af3130d41ac43a92
SantiagoDeliaa <deliasantiago774@gmail.com>
2023-07-16 12:58:21 -0300

    key

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '4axu3f',
+  projectId: '42x1mt',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -10,3 +10,5 @@ module.exports = defineConfig({
     watchForFileChanges: false,
   },
 });
+
+//npx cypress run --record --key 2c7daddc-c499-415b-bb55-63886bde9a43
